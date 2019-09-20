523fa91875f91db525dd452bbe292f87ae3eba27
Alexis Coulombe <acoulombe@globalia.ca>
2019-09-20 15:03:39 -0400

    Refactoring base.php to be more readable + updated composer required php version

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
-// Kickstart the framework
+// Require the framework
 $f3=require('lib/base.php');
-
-$f3->set('DEBUG',1);
-if ((float)PCRE_VERSION<7.9)
-	trigger_error('PCRE version is out of date');
-
-// Load configuration
-$f3->config('config.ini');
 
 $f3->route('GET /',
 	function($f3) {
         $controller = new IndexController($f3);
-        $controller->view();
+				echo $f3->error(500);
+        echo $controller->view();
 	}
 );
 
