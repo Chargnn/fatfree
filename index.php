28db647b298ef39d873a38e344d844d1c5d28446
Alexis Coulombe <acoulombe@globalia.ca>
2019-09-20 15:06:53 -0400

    Hotfix removed error code

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ $f3=require('lib/base.php');
 $f3->route('GET /',
 	function($f3) {
         $controller = new IndexController($f3);
-				echo $f3->error(500);
         echo $controller->view();
 	}
 );
